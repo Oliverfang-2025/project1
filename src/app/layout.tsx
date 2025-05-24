f348b6a4173dfe931aa708caeaffb070b5b5851d
oliver <635800070@qq.com>
2025-05-24 11:41:33 +0800

    添加Dify聊天机器人到网站

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN">
+      <head>
+        <style dangerouslySetInnerHTML={{ __html: `
+          #dify-chatbot-bubble-button {
+            background-color: #1C64F2 !important;
+          }
+          #dify-chatbot-bubble-window {
+            width: 24rem !important;
+            height: 40rem !important;
+          }
+        `}} />
+      </head>
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-grow">{children}</main>
           <Footer />
         </div>
+
+        {/* Dify聊天机器人集成 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.difyChatbotConfig = {
+                token: 'uluSs0xDCF8cY734',
+                systemVariables: {},
+              }
+            `
+          }}
+        />
+        <script
+          src="https://udify.app/embed.min.js"
+          id="uluSs0xDCF8cY734"
+          defer
+        />
       </body>
     </html>
   );
